cacd940866a2969cd6eec88432029984a90e3eb1
Teerat Pitakrat <teerat.pitakrat@gmail.com>
2013-06-21 21:28:50 +0200

    Added more aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,8 +1,26 @@
 # misc
 alias ls='ls --color=auto'
-alias ll='ls -l'
-alias la='ls -la'
-alias ev='evince'
+alias ll='ls -lp'
+alias la='ls -lpa'
+alias lh='ls -lph'
+alias lt='ls -lpatr'
+
+alias ..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
+alias .....='cd ../../../..'
+alias ......='cd ../../../../..'
+alias .......='cd ../../../../../..'
+alias ........='cd ../../../../../../..'
+alias .........='cd ../../../../../../../..'
+
+alias ..2='cd ../..'
+alias ..3='cd ../../..'
+alias ..4='cd ../../../..'
+alias ..5='cd ../../../../..'
+alias ..6='cd ../../../../../..'
+alias ..7='cd ../../../../../../..'
+alias ..8='cd ../../../../../../../..'
 
 # suffix aliases
 #alias -s pdf=evince
@@ -10,6 +28,9 @@ alias ev='evince'
 # global aliases
 #alias -g G='| grep'
 #alias -g L='| less'
+
+# apps
+alias ev='evince'
 
 # arch
 alias y='yaourt'
@@ -57,7 +78,14 @@ alias gf='git ls-files | grep'
 
 # svn
 alias sup='svn up'
-alias sst='svn status'
+alias sss='svn status'
 alias sa='svn add'
 alias sc='svn commit'
 alias sl='svn log -l'
+
+# ssh
+alias zeus='ssh zeus'
+alias pi='ssh pi'
+alias brisbane='ssh brisbane'
+alias brisbane-mildura='ssh brisbane-mildura'
+alias adelaide='ssh adelaide'
