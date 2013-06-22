b56976920b190e727c3450e437b7b3c4c5caa47b
Teerat Pitakrat <teerat.pitakrat@gmail.com>
2013-06-22 19:24:54 +0200

    Removed old codes

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -13,7 +13,6 @@ git_info() {
 }
 
 git_current_branch() {
-	#git_ref=$(git symbolic-ref HEAD 2> /dev/null) || return
 	GIT_BRANCH=${GIT_REF#refs/heads/}
 	echo -n "%{$fg_bold[$GIT_COLOR_BRANCH]%}($GIT_BRANCH)%{$reset_color%}"
 }
@@ -23,29 +22,22 @@ git_status () {
 	echo -n " "
 	#$(git remote update) || return		# takes too long, do it manually!
 	# Check behind
-	#if [[ $(echo ${gitstat} | grep -c "^# Your branch is behind") > 0 ]]; then
 	if echo $gitstat | grep -q "^# Your branch is behind" ; then
 		echo -n "$GIT_STATUS_BEHIND"
 	fi
 	# Check untracked files
-	#if [[ $(echo ${gitstat} | grep -c "^\(# Untracked files:\|# Changed but not updated:\)$") > 0 ]]; then
 	if echo ${gitstat} | grep -q "^\(# Untracked files:\|# Changed but not updated:\)$" ; then
 		echo -n "$GIT_STATUS_UNTRACKED"
 	fi
 	# Check changes to tracked files
-	#if [[ $(echo ${gitstat} | grep -c "^\(# Changes not staged for commit:\)$") > 0 ]]; then
 	if echo ${gitstat} | grep -q "^\(# Changes not staged for commit:\)$" ; then
 		echo -n "$GIT_STATUS_CHANGES"
 	fi
 	# Check staged files
-	#if [[ $(echo ${gitstat} | grep -c "^# Changes to be committed:$") > 0 ]]; then
 	if echo ${gitstat} | grep -q "^# Changes to be committed:$" ; then
 		echo -n "$GIT_STATUS_STAGED"
 	fi
 	# Check ahead
-	#if $(echo "$(git log origin/$(git_branch)..HEAD 2> /dev/null)" | grep '^commit' &> /dev/null); then
-	#	echo -n "$GIT_STATUS_AHEAD"
-	#fi
 	if echo ${gitstat} | grep -q "^# Your branch is ahead of" ; then
 		echo -n "$GIT_STATUS_AHEAD"
 	fi
