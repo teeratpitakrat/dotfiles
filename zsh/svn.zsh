f0b5bc611ebd5ec49890f0095c91770b87bafbbe
Teerat Pitakrat <teerat.pitakrat@gmail.com>
2013-06-22 23:26:55 +0200

    Refactored and remove brackets from svn repo

--- zsh/svn.zsh
+++ zsh/svn.zsh
@@ -5,24 +5,24 @@ SVN_STATUS_UNTRACKED="%{$fg_bold[$SVN_COLOR_STATUS]%}?%{$reset_color%}"
 SVN_STATUS_CHANGES="%{$fg_bold[$SVN_COLOR_STATUS]%}M%{$reset_color%}"
 
 svn_info() {
-	ref=$(svn info 2> /dev/null) || return
-	echo -n " %{$fg_bold[$SVN_COLOR_REPO]%}[$(svn_repo)]%{$reset_color%}%{$fg_bold[$SVN_COLOR_STATUS]%}$(svn_status)%{$reset_color%}"
+	SVN_INFO=$(svn info 2> /dev/null) || return
+	echo -n " $(svn_repo)$(svn_status)"
 }
 
 svn_repo() {
-	svnrepo=$(svn info | sed -n 's/Repository\ Root:\ .*\///p')
-	echo -n "$svnrepo"
+	SVN_REPO=$(echo $SVN_INFO | sed -n 's/Repository\ Root:\ .*\///p')
+	echo -n "%{$fg_bold[$SVN_COLOR_REPO]%}${SVN_REPO}%{$reset_color%}"
 }
 
 svn_status () {
 	svnstat=$(svn status 2>/dev/null | grep '^\(?\|M\|A\)')
 	echo -n " "
 	# Check untracked files
-	if [[ $(echo ${svnstat} | grep -c "^\(?\)") > 0 ]]; then
+	if echo ${svnstat} | grep -q "^\(?\)" ; then
 		echo -n "$SVN_STATUS_UNTRACKED"
 	fi
 	# Check changes
-	if [[ $(echo ${svnstat} | grep -c "^\(M\|A\)") > 0 ]]; then
+	if echo ${svnstat} | grep -q "^\(M\|A\)" ; then
 		echo -n "$SVN_STATUS_CHANGES"
 	fi
 }
