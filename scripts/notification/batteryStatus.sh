d30d3b38623317aeec1eb9282570a0dddad79b89
Teerat Pitakrat <teerat.pitakrat@gmail.com>
2013-07-23 17:21:31 +0200

    Changed battery notification to use notify-send

--- scripts/notification/batteryStatus.sh
+++ scripts/notification/batteryStatus.sh
@@ -8,5 +8,6 @@ THRESHOLD_PERCENTAGE=15
 
 if [[ ( `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 5 -d " "` < $THRESHOLD_TIME ) || ( `echo $BATTINFO | grep Discharging` && `echo $BATTINFO | cut -f 4 -d " " | sed 's/%,//'` < $THRESHOLD_PERCENTAGE ) ]]; then
 	#DISPLAY=:0.0 /usr/bin/notify-send "Low battery" "$BATTINFO"				# ubuntu
-	twmnc -i ~/icons/notification/critical.png --bg '#FF0000' -t "Low battery" -c "$BATTINFO"	# twmn
+	#twmnc -i ~/icons/notification/critical.png --bg '#FF0000' -t "Low battery" -c "$BATTINFO"	# twmn
+	notify-send -t 0 -u low "Low battery" "$BATTINFO"
 fi
