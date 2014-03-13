5523fc43d7b93c67e519f5f949577831a13a6954
Teerat Pitakrat <teerat.pitakrat@gmail.com>
2014-03-13 17:40:47 +0100

    Added gsutil to PATH

--- zsh/export.zsh
+++ zsh/export.zsh
@@ -1,4 +1,5 @@
 export EDITOR=/usr/bin/vim
 export PATH=$PATH:~/bin
+export PATH=$PATH:~/gsutil			# Add path to gsutil (google cloud storage tool)
 export GREP_OPTIONS='--color=auto'	# add color to grep
 export GREP_COLOR='1;36'			# green color
