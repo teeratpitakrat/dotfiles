8115a4d0ccaf7711b53ea266adcbde005b1901b4
Teerat Pitakrat <teerat.pitakrat@gmail.com>
2014-03-13 17:43:23 +0100

    Fixed git status

--- zsh/git.zsh
+++ zsh/git.zsh
@@ -18,7 +18,7 @@ git_current_branch() {
 }
 
 git_status () {
-	gitstat=$(git status 2>/dev/null | grep '\(# Your branch is behind\|# Untracked\|# Changes\|# Changed but not updated:\|# Your branch is ahead of\)') || return
+	gitstat=$(git status 2>/dev/null | grep '\(Your branch is behind\|Untracked\|Changes\|Changed but not updated:\|Your branch is ahead of\)') || return
 	echo -n " "
 	#$(git remote update) || return		# takes too long, do it manually!
 	# Check behind
@@ -26,19 +26,19 @@ git_status () {
 		echo -n "$GIT_STATUS_BEHIND"
 	fi
 	# Check untracked files
-	if echo ${gitstat} | grep -q "^\(# Untracked files:\|# Changed but not updated:\)$" ; then
+	if echo ${gitstat} | grep -q "^\(Untracked files:\|Changed but not updated:\)$" ; then
 		echo -n "$GIT_STATUS_UNTRACKED"
 	fi
 	# Check changes to tracked files
-	if echo ${gitstat} | grep -q "^\(# Changes not staged for commit:\)$" ; then
+	if echo ${gitstat} | grep -q "^Changes not staged for commit:$" ; then
 		echo -n "$GIT_STATUS_CHANGES"
 	fi
 	# Check staged files
-	if echo ${gitstat} | grep -q "^# Changes to be committed:$" ; then
+	if echo ${gitstat} | grep -q "^Changes to be committed:$" ; then
 		echo -n "$GIT_STATUS_STAGED"
 	fi
 	# Check ahead
-	if echo ${gitstat} | grep -q "^# Your branch is ahead of" ; then
+	if echo ${gitstat} | grep -q "^Your branch is ahead of" ; then
 		echo -n "$GIT_STATUS_AHEAD"
 	fi
 }
