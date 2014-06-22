a207688b88a61302b6901a60fe58be39fa03abdc
Teerat Pitakrat <teerat.pitakrat@gmail.com>
2014-06-22 12:27:06 +0200

    Fixed distorted sound for skype

--- zsh/export.zsh
+++ zsh/export.zsh
@@ -3,3 +3,4 @@ export PATH=$PATH:~/bin
 export PATH=$PATH:~/gsutil			# Add path to gsutil (google cloud storage tool)
 export GREP_OPTIONS='--color=auto'	# add color to grep
 export GREP_COLOR='1;36'			# green color
+export PULSE_LATENCY_MSEC=60		# fix distorted sound for skype
