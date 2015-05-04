311c5aaf27c726e14913a0734d78e2eab23c1086
Teerat Pitakrat <teerat.pitakrat@gmail.com>
2015-05-04 10:52:02 +0200

    Fixed home end key in shell

--- zsh/bindkey.zsh
+++ zsh/bindkey.zsh
@@ -4,8 +4,8 @@ bindkey '^[[A' up-line-or-search	# up arrow will search for the same command
 bindkey '^[[B' down-line-or-search	# down arrow will search for the same command
 bindkey "^[[1;5C" emacs-forward-word	#control left
 bindkey "^[[1;5D" backward-word			#control right
-bindkey "^[OH" beginning-of-line		# home
-bindkey "^[OF" end-of-line				# end
+bindkey '^[[1~' beginning-of-line		# home
+bindkey '^[[4~' end-of-line				# end
 bindkey '^R' history-incremental-search-backward	# enable Ctrl+R to search history
 bindkey '\e.' insert-last-word		# Alt+. to repeat the last part of previous command
 bindkey "\e[3~" delete-char			# enable delete key for gnome-terminal
