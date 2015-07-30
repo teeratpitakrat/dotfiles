a107a9b4188b55c240f719da65ee11e7004fc038
Teerat Pitakrat <teerat.pitakrat@gmail.com>
2015-07-30 14:00:15 +0200

    Added tmux session to zsh right prompt

---
+++ zsh/tmux.zsh
@@ -0,0 +1,10 @@
+TMUX_COLOR_DETACHED_SESSION="cyan"
+
+tmux_session() {
+	if which tmux > /dev/null; then
+		numDetachedSessions=$(tmux ls 2>/dev/null | grep -v "attached" | wc -l)
+		if [[ $numDetachedSessions -ge 1 ]]; then
+			echo -n " %{$fg[$TMUX_COLOR_DETACHED_SESSION]%}$numDetachedSessions%{$reset_color%}"
+		fi
+	fi
+}
