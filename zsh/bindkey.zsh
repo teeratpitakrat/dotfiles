e932419da6ba69bb45c9c37c91bd08b5abb7a088
Teerat Pitakrat <teerat.pitakrat@gmail.com>
2015-10-31 15:29:43 +0100

    Change key binding for zsh in gnome-terminal

--- zsh/bindkey.zsh
+++ zsh/bindkey.zsh
@@ -1,11 +1,10 @@
-#bindkey -v		# use vi-mode in the shell
 bindkey -e		# use emacs mode in the shell
 bindkey '^[[A' up-line-or-search	# up arrow will search for the same command
 bindkey '^[[B' down-line-or-search	# down arrow will search for the same command
 bindkey "^[[1;5C" emacs-forward-word	#control left
 bindkey "^[[1;5D" backward-word			#control right
-bindkey '^[[1~' beginning-of-line		# home
-bindkey '^[[4~' end-of-line				# end
+bindkey '^[OH' beginning-of-line		# home
+bindkey '^[OF' end-of-line				# end
 bindkey '^R' history-incremental-search-backward	# enable Ctrl+R to search history
 bindkey '\e.' insert-last-word		# Alt+. to repeat the last part of previous command
 bindkey "\e[3~" delete-char			# enable delete key for gnome-terminal
