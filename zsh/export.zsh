7789f04975206471aeab3e9b42492d6b1cd6c3da
Teerat Pitakrat <teerat.pitakrat@gmail.com>
2017-02-23 16:47:25 +0100

    Add export path for ruby and go in zsh

--- zsh/export.zsh
+++ zsh/export.zsh
@@ -1,7 +1,10 @@
 export EDITOR=/usr/bin/vim
 export PATH=$PATH:~/bin
 export PATH=$PATH:~/gsutil			# Add path to gsutil (google cloud storage tool)
+export PATH=$PATH:~/.gem/ruby/2.3.0/bin	# Add path to RubyGems
 #export GREP_OPTIONS='--color=auto'	# add color to grep
 unset GREP_OPTIONS
 export GREP_COLOR='1;36'			# green color
 export PULSE_LATENCY_MSEC=60		# fix distorted sound for skype
+export GOPATH=~/go
+export PATH="$PATH:$GOPATH/bin"
