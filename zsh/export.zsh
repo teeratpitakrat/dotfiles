eb1d1d5e45c0383ca8fc771d219d0bd80c8d7459
Teerat Pitakrat <teerat.pitakrat@gmail.com>
2018-04-11 12:56:54 +0200

    Config ruby path

--- zsh/export.zsh
+++ zsh/export.zsh
@@ -1,7 +1,7 @@
 export EDITOR=/usr/bin/nvim
 export PATH=$PATH:~/bin
 export PATH=$PATH:~/gsutil			# Add path to gsutil (google cloud storage tool)
-export PATH=$PATH:~/.gem/ruby/2.3.0/bin	# Add path to RubyGems
+export PATH="$PATH:$(ruby -e 'print Gem.user_dir')/bin"
 #export GREP_OPTIONS='--color=auto'	# add color to grep
 unset GREP_OPTIONS
 export GREP_COLOR='1;36'			# green color
