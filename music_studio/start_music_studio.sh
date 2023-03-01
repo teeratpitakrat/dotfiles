134d27dcc4521c1b8c937522c49ee395fbdc91d8
Teerat Pitakrat <teerat.pitakrat@gmail.com>
2023-03-01 21:06:47 +0100

    Update music studio

--- music_studio/start_music_studio.sh
+++ music_studio/start_music_studio.sh
@@ -8,7 +8,8 @@ pacmd load-module module-jack-sink channels=2 # Jack sink #2 for pulseaudio
 pacmd load-module module-jack-source channels=2 # Jack source #2 for something else
 #pacmd load-module module-jack-source channels=2 # Jack source #3 for something else
 
-a2jmidi_bridge & # Alsa 2 Jack MIDI bridge for MIDI device connected to hardware
+a2jmidi_bridge m4 & # Alsa 2 Jack MIDI bridge for MIDI device connected to hardware
+a2jmidi_bridge mpc & # Alsa 2 Jack MIDI bridge for MIDI device connected to hardware
 sleep 2
 
 # Start applications
@@ -22,7 +23,7 @@ pavucontrol &
 sleep 2
 
 i3-msg "workspace 4; append_layout ~/.dotfiles/music_studio/i3wm-layouts/ws-4.json"
-qsampler ~/.dotfiles/music_studio/qsampler/sampler.lscp &
+#qsampler ~/.dotfiles/music_studio/qsampler/sampler.lscp &
 zynaddsubfx &
 qjackctl &
 sleep 10
